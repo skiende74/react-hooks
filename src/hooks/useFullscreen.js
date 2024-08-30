3e3e455a4e2928426539276cf4c503af6d009ed7
skiende74 <skiende74@gmail.com>
2024-08-30 18:39:12 +0900

    feat: make compatibility for other browsers

--- src/hooks/useFullscreen.js
+++ src/hooks/useFullscreen.js
@@ -1,19 +1,40 @@
 import { useRef } from "react";
 
-const useFullscreen = (onFullScreen) => {
+const useFullscreen = (callback) => {
   const element = useRef(null);
 
+  const runCallback = (status) => {
+    if (typeof callback !== "function") return;
+    callback(status);
+  };
   const triggerFull = () => {
     if (element.current === null) return;
-    element.current.requestFullscreen();
-    if (typeof onFullScreen !== "function") return;
-    onFullScreen(true);
+
+    if (element.current.requestFullscreen) {
+      element.current.requestFullscreen();
+    } else if (element.current.mozRequestFullScreen) {
+      element.current.mozRequestFullScreen();
+    } else if (element.current.webkitRequestFullScreen) {
+      element.current.webkitRequestFullScreen();
+    } else if (element.current.msRequestFullScreen) {
+      element.current.msRequestFullScreen();
+    }
+
+    runCallback(false);
   };
 
   const exitFull = () => {
-    document.exitFullscreen();
-    if (typeof onFullScreen !== "function") return;
-    onFullScreen(false);
+    if (document.exitFullScreen) {
+      document.exitFullScreen();
+    } else if (document.mozExitFullscreen) {
+      document.mozExitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+
+    runCallback(false);
   };
 
   return { element, triggerFull, exitFull };
